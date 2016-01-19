dc7c8898ba5f3e3541fdfc2f9235d2ee4900fa76
Jacob B <jacoblb64@gmail.com>
2016-01-19 15:30:38 -0500

    started adding comments

--- A1_Hill_Cipher/matrix_multiplier.vhd
+++ A1_Hill_Cipher/matrix_multiplier.vhd
@@ -1,5 +1,5 @@
 library ieee;
-use ieee.std_logic_1164.all; -- allows use of the std_logic_vector type
+use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
@@ -18,12 +18,14 @@ end matrix_multiplier;
 
 architecture behv of matrix_multiplier is
 
+	-- p and c registers, to be latched
 	signal p1r, p2r, p3r : std_logic_vector(3 downto 0);
 	signal c1r, c2r, c3r : std_logic_vector(7 downto 0); 
 	
 begin
-
-	mult : process(clk) begin
+	
+	-- latch values on the rising edge of the clock
+	latch : process(clk) begin
 		if(rising_edge(clk)) then
 			p1r <= p1;
 			p2r <= p2;
@@ -35,6 +37,7 @@ begin
 		end if;
 	end process;
 	
+	-- calculae outputs one operation at a time
 	c1r  <= (p1r * k11) + (p2r * k21) + (p3r * k31);
 	c2r  <= (p1r * k12) + (p2r * k22) + (p3r * k32);
 	c3r  <= (p1r * k13) + (p2r * k23) + (p3r * k33);
